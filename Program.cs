0a26cbb532edd3a803236273ce1032d40741d39e
Akulassj <92105691+Akulassj@users.noreply.github.com>
2021-10-12 19:41:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ТРПО_6лаб
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
